d6df570a399aba04bd5cc401bed868785f936266
Your Name <zc00gii@gmail.com>
2010-05-21 20:32:25 -0400

    little changes

--- testing/base/buffer.py
+++ testing/base/buffer.py
@@ -1,22 +1,20 @@
 import socket
 class Buffer():
     buffer = []
+    extra = ['']
+    data = ''
     def readBuffer(self):
-        data = ''
-        extra = [""]
-        line = [""]
         try:
             data = extra[0] + self.recv(1024)
             data = data.replace('\r', "")
             extra.pop(0)
             for x in range(len(data.split('\n'))):
                 extra.append(data.split('\n')[x])
-            data = data.split('\n')[0]
-            line = data.split(':')
-            line = [':'.join(line[:2]), ':'.join(line[2:])]
+            line = data.split('\n')[0]
         except socket.error:
             self.close
         return line
+
     def findBuffer(self, line):
         x = 0
         for x in range(len(self.buffer)):
